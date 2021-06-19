b01bb5bb80fc8ae8f40e30cc71c59eb1b38befc9
RafaelRodrigues1 <rafaelx7t@hotmail.com>
2021-06-19 18:45:49 -0300

    Operação listar todos

--- src/main/java/com/project/personapi/controller/PersonController.java
+++ src/main/java/com/project/personapi/controller/PersonController.java
@@ -3,9 +3,11 @@ package com.project.personapi.controller;
 import com.project.personapi.dto.request.PersonDTO;
 import com.project.personapi.dto.response.MessageResponseDTO;
 import com.project.personapi.service.PersonService;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,10 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+    
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
 }
--- src/main/java/com/project/personapi/service/PersonService.java
+++ src/main/java/com/project/personapi/service/PersonService.java
@@ -5,6 +5,8 @@ import com.project.personapi.dto.response.MessageResponseDTO;
 import com.project.personapi.entity.Person;
 import com.project.personapi.mapper.PersonMapper;
 import com.project.personapi.repository.PersonRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,4 +36,14 @@ public class PersonService {
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
     }
+    
+    
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    
 }
