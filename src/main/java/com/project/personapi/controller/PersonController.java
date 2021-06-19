332b454f622f889abccccde455712c7b30a6b7f9
RafaelRodrigues1 <rafaelx7t@hotmail.com>
2021-06-19 19:10:59 -0300

    Operação buscar por ID

--- src/main/java/com/project/personapi/controller/PersonController.java
+++ src/main/java/com/project/personapi/controller/PersonController.java
@@ -2,12 +2,14 @@ package com.project.personapi.controller;
 
 import com.project.personapi.dto.request.PersonDTO;
 import com.project.personapi.dto.response.MessageResponseDTO;
+import com.project.personapi.exception.PersonNotFoundException;
 import com.project.personapi.service.PersonService;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,10 @@ public class PersonController {
     @GetMapping
     public List<PersonDTO> listAll(){
         return personService.listAll();
+    }
+    
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException{
+        return personService.findById(id);
     }
 }
--- src/main/java/com/project/personapi/service/PersonService.java
+++ src/main/java/com/project/personapi/service/PersonService.java
@@ -3,9 +3,11 @@ package com.project.personapi.service;
 import com.project.personapi.dto.request.PersonDTO;
 import com.project.personapi.dto.response.MessageResponseDTO;
 import com.project.personapi.entity.Person;
+import com.project.personapi.exception.PersonNotFoundException;
 import com.project.personapi.mapper.PersonMapper;
 import com.project.personapi.repository.PersonRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -43,6 +45,13 @@ public class PersonService {
         return allPeople.stream()
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+       
+        return personMapper.toDTO(person);
     }
 
     
